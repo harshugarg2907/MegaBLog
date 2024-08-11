cbf544cd212e37d4ac918b289ba3d0199c0efcd0
harshugarg2907 <harshugarg2907@gmail.com>
2024-08-11 12:24:52 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Header from "./header/header";
+import Footer from "./footer/footer";
+import Container from "./container/Container.jsx";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import RTE from "./RTE.jsx";
+import Select from "./Select.jsx";
+import Signup from "./Signup.jsx";
+import Login from "./Login.jsx";
+import PostForm from "./post-form/PostForm.jsx";
+import PostCard from "./PostCard.jsx"
+import Button from "./Button.jsx";
+import AuthLayout from "./AuthLayout.jsx";
+import Input from './Input.jsx';
+export{
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Select,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    Button,
+    AuthLayout,
+    Input
+}
